f6e2e22a677d4b45ab75ae9c75e5ecff76e85831
Sushant Singh Negi <107835408+NegiSushant@users.noreply.github.com>
2022-07-08 08:15:57 +0530

    Add files via upload

---
+++ Leap Year/src/Main.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int year;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the Year. ");
+
+        year = sc.nextInt();
+
+        if(year%4==0)
+        {
+            if(year%100==0)
+            {
+                if(year%400==0)
+                {
+                    System.out.println(year+" is a leap year.");
+                }
+                else
+                {
+                    System.out.println(year+" is not a leap year.");
+                }
+            }
+            else
+            {
+                System.out.println(year+" is not a leap year.");
+            }
+        }
+        else
+        {
+            System.out.println(year+" is not a leap year.");
+        }
+    }
+}
