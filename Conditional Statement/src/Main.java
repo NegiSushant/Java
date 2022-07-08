f35f1cc38645aa47c43a6774cae7b02b5f31cf53
Sushant Singh Negi <107835408+NegiSushant@users.noreply.github.com>
2022-07-08 08:12:27 +0530

    Add files via upload

---
+++ Conditional Statement/src/Main.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int a, b, c;
+
+        Scanner sc= new Scanner(System.in);
+
+        System.out.println("Enter the values of a, b and c ");
+
+        a=sc.nextInt();
+        b=sc.nextInt();
+        c=sc.nextInt();
+
+
+
+        if(a>b && a>c)
+        {
+            System.out.println(a);
+        }
+        else if(b>c)
+        {
+            System.out.println(b);
+        }
+        else
+        {
+            System.out.println(c);
+        }
+    }
+}
