1a5ddde8c66d517efd3b461569697ec557789d00
Sushant Singh Negi <107835408+NegiSushant@users.noreply.github.com>
2022-07-08 08:15:20 +0530

    Add files via upload

---
+++ Young or not/src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int age;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the age ");
+
+        age = sc.nextInt();
+
+        if (age>=14 && age<=55)
+        {
+            System.out.println("You are young.");
+        }
+        else
+        {
+            System.out.println("You are not young");
+        }
+    }
+}
