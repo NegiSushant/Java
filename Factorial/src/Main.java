e6f3719d1502aef0ff8f04704f8ba6542b83ac9c
Sushant Singh Negi <107835408+NegiSushant@users.noreply.github.com>
2022-07-09 06:53:58 +0530

    Add files via upload

---
+++ Factorial/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int n;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the number. ");
+        n = sc.nextInt();
+
+        int fact = 1;
+
+        for(int i=1; i<=n; i++) {
+            fact = fact *i;
+        }
+
+        System.out.println("Factorial of "+n+" is "+fact);
+    }
+}
