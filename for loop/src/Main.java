0be22cb60fc0c3371464f6221c73d30ee4eaf9fa
Sushant Singh Negi <107835408+NegiSushant@users.noreply.github.com>
2022-07-09 06:51:32 +0530

    Add files via upload

---
+++ for loop/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        for(int i=0; i<=10; i++) {
+            System.out.println(i);
+       }
+        for(int j=10; j>=0; j--){
+            System.out.println(j);
+        }
+    }
+}
