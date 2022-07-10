64e1d3d059800e92f7ef76b769dfac39e2f4ff45
Sushant Singh Negi <107835408+NegiSushant@users.noreply.github.com>
2022-07-10 10:14:12 +0530

    Add files via upload

---
+++ Sum of Array/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int A[] = {15, 7, 22, 3, 16};
+
+        int sum = 0;
+
+        for(int i=0; i<A.length; i++){
+            sum = sum + A[i];
+        }
+        System.out.println(sum);
+    }
+}
